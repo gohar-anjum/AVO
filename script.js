54c52675531e7947190b197d4479801f98afcef1
gohar-anjum <goharanjum49@gmail.com>
2024-08-06 02:00:07 +0500

    HomePage File of AVO

---
+++ script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    spaceBetween: 100,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+  
+    swiper.slideNext(100);
