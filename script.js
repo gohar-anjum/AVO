34d35390e05271940eae3224f5494e0cd817cf9c
gohar-anjum <goharanjum49@gmail.com>
2024-08-06 21:03:48 +0500

    HeroSec Structure

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
-// var swiper = new Swiper(".mySwiper", {
-//     spaceBetween: 30,
-//     centeredSlides: true,
-//     autoplay: {
-//       delay: 2500,
-//       disableOnInteraction: false,
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//   });
+var swiper = new Swiper("#HS", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
