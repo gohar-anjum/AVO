5c07c516e5a94d3b193955770f1d37b5f3b5d4ab
gohar-anjum <goharanjum49@gmail.com>
2024-08-06 19:50:57 +0500

    HeroSec Structure

--- script.js
+++ script.js
@@ -11,13 +11,3 @@
 //       clickable: true,
 //     },
 //   });
-const images = ['image1.jpg', 'image2.jpg', 'image3.jpg'];
-let currentImageIndex = 0;
-
-function changeImage() {
-    const header = document.querySelector('header');
-    header.style.backgroundImage = `url(${images[currentImageIndex]})`;
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-}
-
-setInterval(changeImage, 5000); // Change image every 5 seconds
