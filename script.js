44fc5d4722fd697a230469f6e245fc6463246390
gohar-anjum <goharanjum49@gmail.com>
2024-08-06 17:46:32 +0500

    Header AVO

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    spaceBetween: 100,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  });
 
-  
-    swiper.slideNext(100);
+// var swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
+const images = ['image1.jpg', 'image2.jpg', 'image3.jpg'];
+let currentImageIndex = 0;
+
+function changeImage() {
+    const header = document.querySelector('header');
+    header.style.backgroundImage = `url(${images[currentImageIndex]})`;
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+}
+
+setInterval(changeImage, 5000); // Change image every 5 seconds
