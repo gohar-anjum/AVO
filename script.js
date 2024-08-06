712046d78403040e8ee53f8676d06d0e35642a04
gohar-anjum <goharanjum49@gmail.com>
2024-08-06 23:44:32 +0500

    HeroSec silde One Animation Done

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 var swiper = new Swiper(".mySwiper", {
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
+      delay: 4500,
       disableOnInteraction: false,
     },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-  });
+});
+
+  let time = 0; // Initialize the time variable
+  let cue = document.querySelector(".ssr h1"); // Select the element
+  
+  // Create an interval to update the counter
+const interval = setInterval(() => {
+    if (time <= 400) { 
+      cue.innerHTML = `${time}`;
+      time++;
+    } else {
+      clearInterval(interval);
+    }
+}, 6);
