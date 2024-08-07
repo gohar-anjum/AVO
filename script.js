34f00fc9ba91969c8f5e2a6d21fb5d344021e158
gohar-anjum <goharanjum49@gmail.com>
2024-08-08 02:28:34 +0500

    Section4 Reviews Done

--- script.js
+++ script.js
@@ -37,3 +37,13 @@ const timeinterval = setInterval(()=> {
     }
 
 }, 6)
+
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination2",
+    clickable: true,
+  },
+});
