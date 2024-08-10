0013782bee71a1b43da8fc93ca16dbee6a7d647d
gohar-anjum <goharanjum49@gmail.com>
2024-08-11 01:16:23 +0500

    Home Page Completed left with Responsive

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 var swiper = new Swiper(".mySwiper", {
-    effect: "fade",
-    fadeEffect: {
-      crossFade: true,
-    },
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 14500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 
 let time = 0;
 let cue = document.querySelector(".ssr h1");
-const interval = setInterval(()=> {
-    if (time <= 400) { 
-      cue.innerHTML = `${time}`;
-      time++;
-    } else {
-      clearInterval(interval);
-    }
+const interval = setInterval(() => {
+  if (time <= 400) {
+    cue.innerHTML = `${time}`;
+    time++;
+  } else {
+    clearInterval(interval);
+  }
 }, 6);
 let start = 0;
 let cue2 = document.querySelector(".ssr2 h1")
-const timeinterval = setInterval(()=> {
-    if(start<=21){
-        cue2.innerHTML=`${start}`;
-        start++;
-    }
-    else{
-        clearInterval(timeinterval);
-    }
+const timeinterval = setInterval(() => {
+  if (start <= 21) {
+    cue2.innerHTML = `${start}`;
+    start++;
+  }
+  else {
+    clearInterval(timeinterval);
+  }
 
 }, 6)
 
@@ -47,3 +47,14 @@ var swiper = new Swiper(".mySwiper2", {
     clickable: true,
   },
 });
+
+let playvideo = document.querySelector(".playvideo")
+playvideo.addEventListener("click", () => {
+  let videosection = document.querySelector(".videosection")
+  videosection.style.display = "block";
+  let crossvideo = document.querySelector(".videosection i")
+  crossvideo.addEventListener("click", () => {
+    let videosection = document.querySelector(".videosection")
+    videosection.style.display = "none";
+  })
+})
