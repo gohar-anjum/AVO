2d4e31cfb1a79949d07e73730c81eb521fd59a06
gohar-anjum <goharanjum49@gmail.com>
2024-08-17 22:40:54 +0500

    About Us Updated

--- about-us.js
+++ about-us.js
@@ -1,3 +1,11 @@
+document.addEventListener('scroll', ()=>{
+    const mainmenu = document.querySelector("#mainmenu")
+    if (window.scrollY>200){
+      mainmenu.classList.add('scrolled')
+    }else{
+      mainmenu.classList.remove('scrolled')
+    }
+  })
 let imgdiv = document.querySelectorAll("#mimg");
 let icon = document.querySelectorAll(".icon");
 
