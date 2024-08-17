4b485b6291a4993db72ad423e8c104c85a800ad5
gohar-anjum <goharanjum49@gmail.com>
2024-08-17 22:56:54 +0500

    About Us Updated

--- about-us.js
+++ about-us.js
@@ -6,14 +6,67 @@ document.addEventListener('scroll', ()=>{
       mainmenu.classList.remove('scrolled')
     }
   })
-let imgdiv = document.querySelectorAll("#mimg");
-let icon = document.querySelectorAll(".icon");
-
-for (let i = 0; i < imgdiv.length; i++) {
-  imgdiv[i].addEventListener('click', () => {
-    // Iterate through each icon element
-    for (let j = 0; j < icon.length; j++) {
-      icon[j].style.display = "initial";
-    }
-  });
-}
+let mimg1 = document.querySelector(".mimg1");
+let iconsdiv1 = document.querySelector(".mimg1 .iconsdiv");
+mimg1.addEventListener('mouseenter', ()=>{
+    iconsdiv1.style.visibility = 'visible';
+})
+mimg1.addEventListener('mouseleave', ()=>{
+    iconsdiv1.style.visibility = 'hidden';
+})
+let mimg2 = document.querySelector(".mimg2");
+let iconsdiv2 = document.querySelector(".mimg2 .iconsdiv");
+mimg2.addEventListener('mouseenter', ()=>{
+    iconsdiv2.style.visibility = 'visible';
+})
+mimg2.addEventListener('mouseleave', ()=>{
+    iconsdiv2.style.visibility = 'hidden';
+})
+let mimg3 = document.querySelector(".mimg3");
+let iconsdiv3 = document.querySelector(".mimg3 .iconsdiv");
+mimg3.addEventListener('mouseenter', ()=>{
+    iconsdiv3.style.visibility = 'visible';
+})
+mimg3.addEventListener('mouseleave', ()=>{
+    iconsdiv3.style.visibility = 'hidden';
+})
+let mimg4 = document.querySelector(".mimg4");
+let iconsdiv4 = document.querySelector(".mimg4 .iconsdiv");
+mimg4.addEventListener('mouseenter', ()=>{
+    iconsdiv4.style.visibility = 'visible';
+})
+mimg4.addEventListener('mouseleave', ()=>{
+    iconsdiv4.style.visibility = 'hidden';
+})
+let mimg5 = document.querySelector(".mimg5");
+let iconsdiv5 = document.querySelector(".mimg5 .iconsdiv");
+mimg5.addEventListener('mouseenter', ()=>{
+    iconsdiv5.style.visibility = 'visible';
+})
+mimg5.addEventListener('mouseleave', ()=>{
+    iconsdiv5.style.visibility = 'hidden';
+})
+let mimg6 = document.querySelector(".mimg6");
+let iconsdiv6 = document.querySelector(".mimg6 .iconsdiv");
+mimg6.addEventListener('mouseenter', ()=>{
+    iconsdiv6.style.visibility = 'visible';
+})
+mimg6.addEventListener('mouseleave', ()=>{
+    iconsdiv6.style.visibility = 'hidden';
+})
+let mimg7 = document.querySelector(".mimg7");
+let iconsdiv7 = document.querySelector(".mimg7 .iconsdiv");
+mimg7.addEventListener('mouseenter', ()=>{
+    iconsdiv7.style.visibility = 'visible';
+})
+mimg7.addEventListener('mouseleave', ()=>{
+    iconsdiv7.style.visibility = 'hidden';
+})
+let mimg8 = document.querySelector(".mimg8");
+let iconsdiv8 = document.querySelector(".mimg8 .iconsdiv");
+mimg8.addEventListener('mouseenter', ()=>{
+    iconsdiv8.style.visibility = 'visible';
+})
+mimg8.addEventListener('mouseleave', ()=>{
+    iconsdiv8.style.visibility = 'hidden';
+})
