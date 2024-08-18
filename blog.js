8c10e45e06bbd1a013cde9a1b60ce440f49c0073
gohar-anjum <goharanjum49@gmail.com>
2024-08-18 17:57:21 +0500

    Site Completed

--- blog.js
+++ blog.js
@@ -6,3 +6,29 @@ document.addEventListener('scroll', ()=>{
       mainmenu.classList.remove('scrolled')
     }
   })
+  let menubtn1 = document.querySelector(".pages span");
+  let mobilemenue1 = document.querySelector(".mobilemenu");
+  let pagea = document.querySelector(".mmpages a");
+document.addEventListener('scroll', ()=>{
+(window.scrollY>200)
+      menubtn1.addEventListener("click", () => {
+      mobilemenue1.style.backgroundColor = "white"
+      pagea.style.color = "black"
+    }); 
+
+    menubtn1.addEventListener("click", () => {
+      mobilemenue1.style.backgroundColor = "black";
+      pagea.style.color = "#fff";
+    }); 
+})
+
+let menubtn = document.querySelector(".pages span");
+let mobilemenue = document.querySelector(".mobilemenu");
+
+menubtn.addEventListener("click", () => {
+  if (mobilemenue.style.visibility === "visible") {
+    mobilemenue.style.visibility = "hidden";
+  } else {
+    mobilemenue.style.visibility = "visible";
+  }
+});
--- script.js
+++ script.js
@@ -4,8 +4,38 @@ document.addEventListener('scroll', ()=>{
     mainmenu.classList.add('scrolled')
   }else{
     mainmenu.classList.remove('scrolled')
+
   }
 })
+
+let menubtn1 = document.querySelector(".pages span");
+  let mobilemenue1 = document.querySelector(".mobilemenu");
+  let pagea = document.querySelector(".mmpages a");
+document.addEventListener('scroll', ()=>{
+(window.scrollY>200)
+      menubtn1.addEventListener("click", () => {
+      mobilemenue1.style.backgroundColor = "white"
+      pagea.style.color = "black"
+    }); 
+
+    menubtn1.addEventListener("click", () => {
+      mobilemenue1.style.backgroundColor = "black";
+      pagea.style.color = "#fff";
+    }); 
+})
+
+let menubtn = document.querySelector(".pages span");
+let mobilemenue = document.querySelector(".mobilemenu");
+
+menubtn.addEventListener("click", () => {
+  if (mobilemenue.style.visibility === "visible") {
+    mobilemenue.style.visibility = "hidden";
+  } else {
+    mobilemenue.style.visibility = "visible";
+  }
+});
+
+
 var swiper = new Swiper(".mySwiper", {
   effect: "fade",
   fadeEffect: {
